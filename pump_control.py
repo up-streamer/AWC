b8cac48a96c2fb6b700e62cc6893a22e428390ee
Ricardo Santos <up_stream@hotmail.com>
2026-08-18 02:21:39 -0300

    Updated msg, doc __iter attempt, better retry and Auto mode

--- pump_control.py
+++ pump_control.py
@@ -6,20 +6,25 @@ ON = True
 OFF = False
 
 class PumpControl:
-    def __init__(self, startPerct, stopPerct, sampleInterval = 1000):
-        self.startPerct = startPerct
-        self.stopPerct = stopPerct
+    def __init__(self, sampleInterval = 1000):
+        self.startPerct = 0
+        self.stopPerct = 100
+        self.sampleInterval = sampleInterval
         self.headTkLevel = 0
         self.headTkErr = 0
-        self.flowOk = True
+        self.flowOk = 0
         self.err = 0
         self.mode = 'Auto'
         self.pumpCommand = 'OFF'
         self.pump = Pin(2, Pin.OUT)
         self.pump.off()
-        if startPerct >= stopPerct:
-            self.err = 1           #Setup limits inverted
-        asyncio.create_task(self._run(sampleInterval))
+
+    def start(self):
+        asyncio.create_task(self._run(self.sampleInterval))
+
+#     def __iter__(self):  # Await 1st reading
+#         await asyncio.sleep_ms(5000) ### New attempt to add __iter__
+#         yield from asyncio.sleep(0)
 
     def _pumpSwitch(self, sw):
         if sw:
@@ -30,13 +35,13 @@ class PumpControl:
             self.pumpCommand = 'OFF'
 
     async def _retry(self, sampleInterval):
-        self.err = 3 #Retry
+        self.err = 2 #Retry
+        self._pumpSwitch(OFF)
         for attempt in range(3):
             for delay in range (10): #Was 15
                 if self.mode == 'Auto' and not self.headTkErr:
                     await asyncio.sleep_ms(sampleInterval)
                 else:
-                    self._pumpSwitch(OFF)
                     return
 
             self._pumpSwitch(ON)
@@ -48,49 +53,58 @@ class PumpControl:
                     self._pumpSwitch(OFF)
                     return
 
-            if self.flowOk:
+            if self.flowOk == 0: # Have flow! All good.
                 self.err = 0
                 return    # Stop retries
             else:
                 self._pumpSwitch(OFF)
 
-        self.err = 5 #Time out no flow
-        self._pumpSwitch(OFF)
+        self.err = 4 #Time out no flow
         return
 
     async def _run(self, sampleInterval):
+        await asyncio.sleep_ms(6000)
         while True:
             levelPerct = float(self.headTkLevel)
-            err = self.err + self.headTkErr
+            if self.headTkErr and (self.pumpCommand == 'ON'):
+                self.err = 3 
+            err = self.err + self.flowOk
 
             if self.mode == 'Auto':
-                if err == 0  and self.flowOk == True:
+                if levelPerct >= self.stopPerct:
+                        if self.pumpCommand == 'ON':
+                            self._pumpSwitch(OFF)
+                if not err:   
                     if levelPerct <= self.startPerct:
                         if self.pumpCommand == 'OFF':
                             self._pumpSwitch(ON)
-                    elif levelPerct >= self.stopPerct:
-                        if self.pumpCommand == 'ON':
-                            self._pumpSwitch(OFF)
+                            for delay in range (10):
+                                if not (self.mode == 'Manual'):
+                                    await asyncio.sleep_ms(sampleInterval)
                 else:
-                    if self.headTkErr:
-                        self.err = 4
-
-                    if self.err == 2:
+                    if self.err == 1:
                         self.err = 0 # Reset Caution message
+                        self._pumpSwitch(OFF)
 
-                    self._pumpSwitch(OFF)
-
-                    if not (err or self.flowOk) and levelPerct <= self.startPerct:
+                    if (not self.headTkErr) and (self.flowOk == 1) and self.pumpCommand == 'ON': 
                         await self._retry(sampleInterval)
+                    else:
+                        self._pumpSwitch(OFF)
 
-            if (self.mode == 'Completar') and (self.pumpCommand == 'ON'):      
-                self.pump.on()
- 
+            # Out of Auto mode #
+            if (self.mode == 'Completar') and (self.pumpCommand == 'ON'): # Hack, necessary to activate pump in 'Completar'
+                self.mode = 'Auto'
+                self._pumpSwitch(ON)
+                for delay in range (10):
+                    if not (self.mode == 'Manual'):
+                        await asyncio.sleep_ms(sampleInterval)
+
             if self.mode == 'Manual':
                 if self.pumpCommand == 'OFF':
                     self.pump.off()
                     self.err = 0 # Cleared errors.
                 if self.pumpCommand == 'ON':
-                    self.pump.on()
-                    self.err = 2 # Caution! Pump running without protection
+                    self.pump.on() 
+                    self.err = 1 # Caution msg Pump running in manual mode
+
             await asyncio.sleep_ms(sampleInterval)
