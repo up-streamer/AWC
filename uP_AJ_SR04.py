d585d6db41c2c8dfd4472013333d1880f238a2cf
Ricardo Santos <up_stream@hotmail.com>
2026-08-18 02:23:47 -0300

    Cleanup, moved calc to _updMeasurements, diff error control for udtMeas.

--- uP_AJ_SR04.py
+++ uP_AJ_SR04.py
@@ -25,21 +25,21 @@ class AJ_SR04:
             self.uart = UART(1, 9600, bits=8, parity=None, stop=1, tx=18, rx=5)
         else:
             self.uart = UART(2, 9600, bits=8, parity=None, stop=1, tx=17, rx=16) # Was UART(2...
-            
+
     def start(self):
         asyncio.create_task(self._run(self.sampleInterval))
         
     def __iter__(self):  # Await 1st reading
         while self.distance is None:
             yield from asyncio.sleep(0)
-            
+
     async def _run(self, sampleInterval):
         self.measurements = Measurements(self.max_distance, self.min_distance, self.max_volume)
         if (sampleInterval - _PAUSE_MS) <= 0:
             sampleInterval = 0
         else:
             sampleInterval = sampleInterval - _PAUSE_MS
-            
+
         while True:
             await asyncio.sleep_ms(sampleInterval)
             self.distance = await self._updDistance()
@@ -47,7 +47,6 @@ class AJ_SR04:
                 self.measurements.__updMeasurements (self.measurements.__average(self.distance))
                 if self.err == 0: # Error msg priority sensor error > measurements error
                     self.err = self.measurements.err
-            self.measurements.err
                     
     async def _updDistance(self):
         self.uart.write(b'\x01')
@@ -57,18 +56,14 @@ class AJ_SR04:
             # Read 4 the characters to raw_distance variable
             raw_distance = self.uart.read()
             
-            #print("Raw Distance = " + str(raw_distance))
             checkData = (raw_distance[0]+raw_distance[1]+raw_distance[2]+1) & 0x00ff
-            #print((raw_distance[0]+raw_distance[1]+raw_distance[2]+1) & 0x00ff)
-            #print("sum: " + str(raw_distance[3]))
-            #print("checkData =" + str(checkData))
             if checkData == raw_distance[3]:
                 hi_data = raw_distance[1] << 8
                 lo_data = raw_distance[2]
                 rawDistance = hi_data + lo_data
                 if rawDistance <= 10000:
                     self.err = 0
-                    self._lastGoodDistance = self.distance
+                    self._lastGoodDistance = rawDistance
                     return rawDistance
                 else:
                     self.err = 2 # No echo detected
@@ -76,7 +71,7 @@ class AJ_SR04:
         else:
             self.err = 1 # Data Check error, CRC fail
             return self._lastGoodDistance
-                    
+
 class Measurements:
     def __init__(self, max_distance, min_distance, max_volume):
         self.level = 0
@@ -87,37 +82,23 @@ class Measurements:
         self.min_distance = min_distance # Max level
         self.max_level = self.max_distance - self.min_distance
         self.tolerance = (self.max_distance - self.min_distance) * 0.05
-        self._lastGoodlevel = 0
         self.max_len = 10
         self.deck = deque((), self.max_len) #Rolling average double end queue
         self.err = 0
-          
-    def __updMeasurements(self, distance):
-        
-        def calc(dist):
-            if (self.err == 0):
-                if (dist < (self.max_distance + self.tolerance)) and (dist > (self.min_distance - self.tolerance)): # Good reading
-                    level = self.max_distance - dist
-                    self._lastGoodlevel = level
-                    return level
-                else:
-                    self.err = 4  # Out of tolerance
-                    return self._lastGoodlevel
-            else:
-                return self._lastGoodlevel
-                
-        self.level = calc(distance)
-        
-        proportion = (self.level / self.max_level ) 
-        self.percentage = '{:.1f}'.format(proportion * 100)
-        self.volume = '{:.1f}'.format(proportion * self.max_volume)
-        
+
+    def __updMeasurements(self, dist):
+        self.err = 0
+        if (dist < (self.max_distance + self.tolerance)) and (dist > (self.min_distance - self.tolerance)): # Good reading
+            self.level = self.max_distance - dist
+            proportion = (self.level / self.max_level ) 
+            self.percentage = '{:.1f}'.format(proportion * 100)
+            self.volume = '{:.1f}'.format(proportion * self.max_volume)
+        else:
+            self.err = 3  # Out of tolerance
+
     # Rolling Average #
     def __average(self, dist):
         self.deck.append(dist)
         average = round((sum(self.deck) / len(self.deck)))
         #self.deck.append(average)
-        print(" Dist is = ..." + str(dist))
-        print(" Average is = ..." + str(average))
         return average
-        
