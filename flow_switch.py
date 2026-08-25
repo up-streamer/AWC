6e7bcd9bf7a3a2bc626abb798d0054dcbc37dcbc
Ricardo Santos <up_stream@hotmail.com>
2026-08-25 04:23:12 -0300

    Cleanup

--- flow_switch.py
+++ flow_switch.py
@@ -7,27 +7,26 @@ class flowSw:
         self.pumpCmd = 'OFF'
         self.err = 0
         self.sampleInterval = sampleInterval
-        self.flowSw = Pin(4, Pin.IN, Pin.PULL_UP) # Still to Check
-        
+        self.flowSw = Pin(4, Pin.IN, Pin.PULL_UP)
+
     def start(self):
         asyncio.create_task(self._run(self.sampleInterval))
 
     async def _run(self, sampleInterval):
         _flowSw = False
-        while True:                         ### Modification try to operate more reliable...
+        while True:
             if self.pumpCmd == 'ON':
                 await asyncio.sleep_ms(sampleInterval) # Delay time to check
-                _flowSw = self.flowSw.value() ###
-                if _flowSw and self.pumpCmd == 'ON': ### Removed ++ True on _flowSw
+                _flowSw = self.flowSw.value()
+                if _flowSw and self.pumpCmd == 'ON':
                     self.err = 0
                 else:
                     self.err = 1 # Waiting flowing on headTk
             else:
-                _flowSw1st = self.flowSw.value()  ###
+                _flowSw1st = self.flowSw.value()
                 await asyncio.sleep_ms(sampleInterval) # Delay time to check
-                #_flowSw2nd = self.flowSw.value()
-                _notFlowSw = not (_flowSw1st + self.flowSw.value()) ### new 
-                if _notFlowSw and self.pumpCmd == 'OFF': # Pump is still OFF
+                _notFlowSw = not (_flowSw1st + self.flowSw.value())
+                if _notFlowSw and self.pumpCmd == 'OFF': # Check pump is still OFF?
                     self.err = 0
                 elif (not _notFlowSw) and self.pumpCmd == 'OFF': ### To cover other possibilites...
                     self.err = 2 # Flow sensor fault            ### Pump turned on during await 
